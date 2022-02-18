9e7b2d50e5a578da114ad18cd2a14af4db386c2d
RayBasilio123 <ray.basilio@aluno.ufop.edu.br>
2022-02-18 11:54:19 -0300

    add_png

--- jogos.py
+++ jogos.py
@@ -8,7 +8,7 @@ def escolhe_jogo():
 
     print("(1) Forca (2) Adivinhação")
 
-    jogo = int(input("Qual jogo? "))1
+    jogo = int(input("Qual jogo? "))
 
     if(jogo == 1):
         print("Jogando forca")
